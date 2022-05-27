0eea42ebba1d22006b600ccee9431821b2f3ed46
danjerezTdf <104039810+danjerezTdf@users.noreply.github.com>
2022-05-27 15:28:56 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+/*Encontrar el Número Máximo
+• Leer valores hasta que se introduzca un cero (0)
+• El usuario puede introducir valores positivos y negativos
+• Encontrar el máximo de los elementos que se introdujeron
+• Analizar cómo cambia el programa para hallar el mínimo*/
+
 let numeroIngresado: number = Number(prompt(`ingrese valor`));
 let max: number = numeroIngresado;
 //let min: number = numeroIngresado;
